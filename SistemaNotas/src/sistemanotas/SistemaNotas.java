5d29a0e52d86381716cc1d6f0d0e2ad548f3cdac
Julian Gonzalez <santygonza1904@gmail.com>
2024-11-07 23:37:10 -0500

    Conexion GIT

--- SistemaNotas/src/sistemanotas/SistemaNotas.java
+++ SistemaNotas/src/sistemanotas/SistemaNotas.java
@@ -3,7 +3,7 @@ package sistemanotas;
 import sistemanotas.InterefacesGraficas.Login;
 
 public class SistemaNotas {
-
+    //COnexion GIT
     public static void main(String[] args) {
         new Login().setVisible(true);
     }
